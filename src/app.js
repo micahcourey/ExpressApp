7b1acb91d06e23b0ea9c36043ddc00cecf7b4349
micahcourey <micahjcourey@gmail.com>
2015-12-11 17:53:55 -0800

    Use Express middleware to serve static files in development

--- src/app.js
+++ src/app.js
@@ -5,8 +5,13 @@ var express = require('express'),
 
 var app = express();
 
+app.use('/static', express.static(__dirname + '/public'))
+
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/templates')
+
 app.get('/', function(req, res) {
-  res.send("<h1>Hello Everybody</h1>");
+  res.render("index");
 });
 
 app.get('/blog/:title?', function(req, res) {
